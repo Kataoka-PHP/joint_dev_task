5d0d8e37f0da09345249f4ee6a279b0781fa39e1
Kataoka-PHP <kataoka.soma9@gmail.com>
2021-07-28 07:41:05 +0900

    PHP　応用課題21を修正

--- task.php
+++ task.php
@@ -456,18 +456,22 @@ print("#####q21#####".PHP_EOL);
 <?php
 
 for ($i = 1; $i <= 30; $i++) {
-  if ($i % 3 ==0 && $i % 5 ==0) {
-    echo 'FizzBuzz'.PHP_EOL;
-  }elseif ($i % 3 ==0 && $i % 7 ==0) {
-    echo 'FizzHoge'.PHP_EOL;
-  }elseif ($i % 3 == 0) {
-    echo 'Fizz'.PHP_EOL;
-  }elseif ($i % 5 ==0) {
-    echo 'Buzz'.PHP_EOL;
-  }elseif ($i % 7 ==0) {
-    echo 'Hoge'.PHP_EOL;
-  }else{
-    echo $i.PHP_EOL;
+    if ($i % 3 ==0 && $i % 5 ==0 && $i % 7 ==0) {
+        echo 'FizzBuzzHoge'.PHP_EOL;
+    }elseif ($i % 5 ==0 && $i % 7 ==0) {
+        echo 'BuzzHoge'.PHP_EOL;
+    }elseif ($i % 3 ==0 && $i % 7 ==0) {
+        echo 'FizzHoge'.PHP_EOL;
+    }elseif ($i % 3 ==0 && $i % 5 ==0) {
+        echo 'FizzBuzz'.PHP_EOL;
+    }elseif ($i % 7 ==0) {
+        echo 'Hoge'.PHP_EOL; 
+    }elseif ($i % 5 ==0) {
+        echo 'Buzz'.PHP_EOL;
+    }elseif ($i % 3 == 0) {
+        echo 'Fizz'.PHP_EOL; 
+    }else{
+        echo $i.PHP_EOL;
     }
 }
     
